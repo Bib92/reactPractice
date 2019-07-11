5e1fa3e882fd7d8c62b1419864b5d5141ea71403
Austin Burton <52255334+Bib92@users.noreply.github.com>
2019-07-10 18:10:12 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Routing from './components/Routing'
+
+function App() {
+  return (
+    <div className="App">
+<p>hello</p> 
+<Routing/>
+</div>
+
+  );
+}
+
+export default App;
